bf9410ed978c38d347da1c29cb3d996362dc41a4
gasparjuliano <gasparjuliano@gmail.com>
2021-11-14 19:27:59 -0300

    Commented "html return" from Class Terminar action. Add more nlu examples.

--- actions/actions.py
+++ actions/actions.py
@@ -164,8 +164,9 @@ class Terminar(Action):
 			return_values_img = ret
 			game.terminate()
 			dispatcher.utter_message(text="Cartas na Mesa!")
-			dispatcher.utter_message(custom=return_values_img, parse_mode='html')
-			dispatcher.utter_custom_json({'text': return_values_img, 'parse_mode': 'html'})
+			dispatcher.utter_message(text=return_values_img)
+			#dispatcher.utter_message(custom=return_values_img, parse_mode='html')
+			#dispatcher.utter_message(json_message={'text': return_values_img, 'parse_mode': 'html'})
 
 		return [
 			SlotSet('game', jsonpickle.encode(game))
